72a16101a2b83b4dc6e2a98228947c63386c3fb6
himanshunegi0007 <himanshunegi1608@gmail.com>
2019-12-15 11:01:12 +0530

    My_Selenium_Code

---
+++ SuiteRunner.java
@@ -0,0 +1,11 @@
+package MyFramework;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({junit.class,junit_IE.class,junitOpera.class})
+
+public class SuiteRunner {
+
+}
---
+++ junitOpera.java
@@ -0,0 +1,59 @@
+package MyFramework;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class junitOpera {
+	public static WebDriver driver;
+	
+	@Test
+	public void sampleTest() {
+		//System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		// WebDriver driver=new FirefoxDriver();
+		 driver = new OperaDriver();
+
+		driver.get("https://www.seleniumhq.org/");
+
+		// System.setProperty("webdriver.gecko.driver","C:\Users\himanshu\workspace\seleniumproject\lib\gikodriver");
+
+	}
+
+	@Test
+	public void nextMethod() {
+		System.out.println("this is second method");
+
+	}
+
+	@Before
+	public void login() {
+		System.out.println("inside before");
+	}
+
+	@After
+	public void logout() {
+		System.out.println("inside after");
+	}
+
+	@BeforeClass
+	public static void launch() {
+		System.out.println("inside after class");
+	}
+	
+	@AfterClass
+	
+	
+		public static void terminate() {
+			System.out.println("inside after class");
+		driver.quit();
+	}
+	
+	
+}
